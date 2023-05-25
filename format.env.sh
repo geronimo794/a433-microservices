a62a4ecf913c69822335118970273d691106b0d0
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-05-25 15:16:43 +0700

    fix: Format env.sh

---
+++ format.env.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+# COPY this file to current directory
+# RENAME it to env.sh
+# FILL all the needed value
+
+# Define repo and tag for docker image
+IMAGE_REPO_NAME=item-app
+IMAGE_TAG=v1
+
+# Define username and repo name
+GITHUB_USERNAME=
+
+# Github password or personal access token
+# Personal access token is better
+GITHUB_PAT=
+
+# Set default image registry github
+GITHUB_REGISTRY=ghcr.io
