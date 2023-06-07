e498464b8819879b52e0483318bb0d7fdf33c021
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-06-07 23:01:33 +0700

    fix: Mongodb node port

---
+++ clear.sh
@@ -0,0 +1,32 @@
+#/bin/bash
+
+# Delete front end
+kubectl delete Deployment frontend-deployment -n karsa
+sleep 5
+kubectl delete Service frontend-service -n karsa
+sleep 5
+
+# Delete back end
+kubectl delete Deployment backend-deployment -n karsa
+sleep 5
+kubectl delete Service backend-service -n karsa
+sleep 5
+
+# Delete database
+kubectl delete StatefulSet mongo-ss -n karsa
+sleep 5
+kubectl delete ConfigMap mongo-cm -n karsa
+sleep 5
+kubectl delete Service mongo-service -n karsa
+sleep 5
+kubectl delete Secret mongo-secret -n karsa
+sleep 5
+
+# NO NEED DELETE PC IT TAKES TIME
+# kubectl delete PersistentVolumeClaim mongo-pvc -n karsa
+# sleep 5
+# kubectl delete PersistentVolume mongo-pv -n karsa
+# sleep 5
+
+
+
