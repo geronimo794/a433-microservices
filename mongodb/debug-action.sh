03e56192f0965f4a1e84a47199bf26625d00d716
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-06-07 16:23:18 +0700

    fix: Rename name and folder

--- mongodb/debug-action.sh
+++ mongodb/debug-action.sh
@@ -1,10 +1,30 @@
 #/bin/bash
 
-# Install secret
-kubectl delete ConfigMap mongo-config-map -n karsa
+# Clear all apply
 kubectl delete StatefulSet mongodb-stateful-set -n karsa
+kubectl delete ConfigMap mongo-config-map -n karsa
+kubectl delete Service mongodb-service -n karsa
+kubectl delete Secret mongo-secret -n karsa
+kubectl delete PersistentVolumeClaim mongo-pvc -n karsa
+kubectl delete PersistentVolume mongo-pv -n karsa
+
+# Checking resource
+kubectl get StatefulSet -n karsa
+kubectl get ConfigMap -n karsa
+kubectl get Service -n karsa
+kubectl get Secret -n karsa
+kubectl get PersistentVolumeClaim -n karsa
+kubectl get PersistentVolume -n karsa
+
+# Apply content
+kubectl apply -f mongo-pv-pvc.yml
+kubectl apply -f mongo-secret.yml
 kubectl apply -f mongo-configmap.yml
+kubectl apply -f mongo-service.yml
 kubectl apply -f mongo-stateful-set.yml
+
+# kubectl apply -f mongo-configmap.yml
+# kubectl apply -f mongo-stateful-set.yml
 
 # # Install PV and PVC
 # kubectl apply -f mongo-pv-pvc.yml
