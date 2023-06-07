80485a606e41139ef364dc6f969fab2db396e38c
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-06-07 22:03:15 +0700

    feat: Backend deployment

--- backend/debug-action.sh
+++ backend/debug-action.sh
@@ -11,8 +11,13 @@ kubectl get Service -n karsa
 # Apply content
 kubectl apply -f karsajobs-deployment.yml
 sleep 10
+kubectl get Deployment -n karsa
+sleep 5
+
 kubectl apply -f karsajobs-service.yml
 sleep 10
+kubectl get Service -n karsa
+sleep 5
 
 # Debuging commands
 # kubectl describe
