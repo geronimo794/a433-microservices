9d3ccd9e6a32ed90760cffb84f430aa215dc949d
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-06-08 12:44:15 +0700

    fix: All commenct

--- mongodb/debug-action.sh
+++ mongodb/debug-action.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 
-# Clear all apply
+# Script untuk mempermudah pengetesan
+
+# Clear all configuration apply
 kubectl delete StatefulSet mongo-ss -n karsa
 kubectl delete ConfigMap mongo-cm -n karsa
 kubectl delete Service mongo-service -n karsa
@@ -17,16 +19,44 @@ kubectl get PersistentVolumeClaim -n karsa
 kubectl get PersistentVolume -n karsa
 
 # Apply content
+# Membuat PV dan PVC untuk storage
 kubectl apply -f mongo-pv-pvc.yml
+# Tunggu 10 detik minimal untuk menunggu kubernetes bekerja
 sleep 10
-kubectl apply -f mongo-secret.yml
-sleep 10
-kubectl apply -f mongo-configmap.yml
-sleep 10
-kubectl apply -f mongo-service.yml
-sleep 10
-kubectl apply -f mongo-stateful-set.yml
+# Memperlihatkan konfigurasi yang telah dibuat
+kubectl get PersistentVolumeClaim -n karsa
+kubectl get PersistentVolume -n karsa
+sleep 5
 
+# Membuat secret untuk deployment nanti
+kubectl apply -f mongo-secret.yml
+# Tunggu 10 detik minimal untuk menunggu kubernetes bekerja
+sleep 10
+kubectl get Secret -n karsa
+sleep 5
+
+# Membuat configmap untuk deployment nanti
+kubectl apply -f mongo-configmap.yml
+# Tunggu 10 detik minimal untuk menunggu kubernetes bekerja
+sleep 10
+kubectl get ConfigMap -n karsa
+sleep 5
+
+# Membuat deployment dengan type statefulset
+kubectl apply -f mongo-stateful-set.yml
+# Tunggu 10 detik minimal untuk menunggu kubernetes bekerja
+sleep 10
+kubectl get StatefulSet -n karsa
+sleep 5
+
+# Membuat service untuk statefulset yang telah dibuat
+kubectl apply -f mongo-service.yml
+# Tunggu 10 detik minimal untuk menunggu kubernetes bekerja
+sleep 10
+kubectl get Service -n karsa
+sleep 5
+
+# Jika butuh debugging mungkin bisa menggunakan ini
 # Debuging commands
 # kubectl describe
 # kubectl get events
