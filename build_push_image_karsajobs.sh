83c449c372b5fadd584a4955b3b7f3a0cb9c3ea7
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-06-08 11:23:40 +0700

    fix: PAT Secure

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -7,7 +7,7 @@ set -x
 set -o allexport
 # If .env not exist then use format.env
 if [ ! -f .env ]; then
-	source local.env
+	source format.env
 else
 	source .env
 fi
@@ -16,22 +16,22 @@ set +o allexport
 # Hide all command and variable value again
 set +x
 
-# # Build image from Docker file with var $KARSA_JOB_BE_REPO_NAME and tag $KARSA_JOB_TAG
-# # You can see it from .env configuration
-# docker build --pull --rm -f "Dockerfile" -t $KARSA_JOB_BE_REPO_NAME:$KARSA_JOB_TAG "."
+# Build image from Docker file with var $KARSA_JOB_BE_REPO_NAME and tag $KARSA_JOB_TAG
+# You can see it from .env configuration
+docker build --pull --rm -f "Dockerfile" -t $KARSA_JOB_BE_REPO_NAME:$KARSA_JOB_TAG "."
 
-# # Show all list of docker iamge
-# docker image ls
+# Show all list of docker iamge
+docker image ls
 
-# # Define full new image name, with registry, namespace, repo
-# FULL_NEW_IMAGE_NAME=$GITHUB_REGISTRY/$GITHUB_USERNAME/$KARSA_JOB_BE_REPO_NAME:$KARSA_JOB_TAG
+# Define full new image name, with registry, namespace, repo
+FULL_NEW_IMAGE_NAME=$GITHUB_REGISTRY/$GITHUB_USERNAME/$KARSA_JOB_BE_REPO_NAME:$KARSA_JOB_TAG
 
-# # Rename previous image repo with new repo and tag
-# docker tag $KARSA_JOB_BE_REPO_NAME:$KARSA_JOB_TAG $FULL_NEW_IMAGE_NAME
+# Rename previous image repo with new repo and tag
+docker tag $KARSA_JOB_BE_REPO_NAME:$KARSA_JOB_TAG $FULL_NEW_IMAGE_NAME
 
 # Login with saved Personal Access Token and Github Username
 # You can configure it via .env file
 echo $GITHUB_PAT | docker login ghcr.io --username $GITHUB_USERNAME --password-stdin
-# echo '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' | docker login ghcr.io --username geronimo794 --password-stdin
-# # Push the image to github registry
-# docker push $FULL_NEW_IMAGE_NAME
+
+# Push the image to github registry
+docker push $FULL_NEW_IMAGE_NAME
