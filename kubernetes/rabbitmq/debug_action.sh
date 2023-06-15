53da8e6eaf8ae76f8a3c612326081dcd7090c7df
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-06-15 23:48:49 +0700

    fix: Kubernetes rabbitmq

--- kubernetes/rabbitmq/debug_action.sh
+++ kubernetes/rabbitmq/debug_action.sh
@@ -1,15 +1,19 @@
 #/bin/bash
 
-helm install rabbitmq \
-	--set auth.username=admin,auth.password=secretpassword \
-	oci://registry-1.docker.io/bitnamicharts/rabbitmq \
-	-n async
+# Install rabbitmq
+helm install rabbitmq oci://registry-1.docker.io/bitnamicharts/rabbitmq -n async
 
-# # Uninstall
+# Uninstall rabbitmq
 # helm uninstall rabbitmq -n async
 
-# # Install local expose rabbitmq
+# Install local expose rabbitmq
 # kubectl apply -f rabbitmq-local-service.yaml
 
 # Or you can use port forward
 # kubectl port-forward --namespace async svc/rabbitmq 15672:15672
+
+# Get credential
+# echo "Username      : admin"
+# echo "Password      : $(kubectl get secret --namespace async rabbitmq -o jsonpath="{.data.rabbitmq-password}" | base64 -d)"
+# echo "ErLang Cookie : $(kubectl get secret --namespace async rabbitmq -o jsonpath="{.data.rabbitmq-erlang-cookie}" | base64 -d)"
+# Saved password: 9iSvqNLFiZNvAzZW
