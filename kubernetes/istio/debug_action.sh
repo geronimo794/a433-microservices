a3c0409fa3edbc4abdd5b87251c2fc8b09eef32b
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-06-16 10:13:27 +0700

    fix: Install export

--- kubernetes/istio/debug_action.sh
+++ kubernetes/istio/debug_action.sh
@@ -6,3 +6,6 @@ istioctl install --set profile=demo -y
 
 # Making this installation the default for injection and validation.
 # Error: failed to create revision tag: failed to apply tag manifests to cluster: failed applying manifest /var/folders/y5/r4jkd8ms3x32_sql383p82gw0000gn/T/revision-tag-manifest-502709870: Get "https://kubernetes.docker.internal:6443/openapi/v2?timeout=32s": context deadline exceeded (Client.Timeout exceeded while awaiting headers)
+
+# Install export
+# export PATH="$PATH:/Users/achrozikin/Documents/development/dicoding/Belajar-Membangun-Arsitektur-Microservices/Proyek-Deploy-Aplikasi-Item-App-dengan-Docker-Compose/a433-microservices/kubernetes/istio/istio-1.18.0/bin"
