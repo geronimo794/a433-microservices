99d8713e50d9a0a30d0da27d0600da1ffd015164
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-06-16 10:12:47 +0700

    fix: Install istion

---
+++ kubernetes/istio/debug_action.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+istioctl install --set profile=demo -y
+
+# kubectl label namespace default istio-injection=enabled
+
+# Making this installation the default for injection and validation.
+# Error: failed to create revision tag: failed to apply tag manifests to cluster: failed applying manifest /var/folders/y5/r4jkd8ms3x32_sql383p82gw0000gn/T/revision-tag-manifest-502709870: Get "https://kubernetes.docker.internal:6443/openapi/v2?timeout=32s": context deadline exceeded (Client.Timeout exceeded while awaiting headers)
